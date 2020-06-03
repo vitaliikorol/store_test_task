912b0a79986438768eb19531b474bc4721ad164d
Vitalii Korol <vitaliikorol@192-168-0-100.rdsnet.ro>
2020-06-03 16:39:26 +0300

    sorting finalized

--- src/Main/main.tsx
+++ src/Main/main.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useMemo, useState} from "react";
+import React, {useCallback, useContext, useEffect, useMemo, useState} from "react";
 import './main.scss'
 import {SideBar} from "../SideBar/sideBar";
 import {ClothesFromServer} from "../Additional/api";
@@ -9,47 +9,53 @@ export const Main = () => {
   const [clothes, setClothes] = useState([])
   const [activeView, setActiveView] = useState('All');
   const [viewSort, setViewSort] = useState(false);
+  const [sortType, setSortType] = useState('')
   const goodsFromServer = useContext(ClothesFromServer);
 
   useEffect(() => {
-    goodsFromServer.then(data => setClothes(data));
+   goodsFromServer.then(data => setClothes(data))
   }, [goodsFromServer])
 
-  useEffect(() => {
 
-  }, [viewSort]);
+  const sortFunction = (array: ClothItem[], type: string) => {
+    switch (type) {
+      case 'Price_asc':
+        return array.sort((a, b) => (b.price - a.price))
+      case 'Price_desc':
+        return array.sort((a, b) => (a.price - b.price))
+      case 'Popularity':
+        return array.sort((a, b) => (a.popularity - b.popularity))
+      case 'Newest':
+        return array.sort((a, b) => (a.age - b.age))
+    }
+  }
 
-
-  const sortedClothes = useMemo(() => {
+  const clothAsPerView = useCallback(() => {
     return activeView === 'All'
       ? clothes
       : clothes.filter((el: ClothItem) => el.type === activeView)
   }, [clothes, activeView])
 
-
-  //
-  // const defineClothes = () => {
-  //   switch (activeView) {
-  //     case 'All':
-  //       return clothes;
-  //     default:
-  //       return sortedClothes;
-  //   }
-  // }
-
+  const sortedClothes = useMemo(() => {
+    return sortType === ''
+    ? clothAsPerView()
+      : sortFunction(clothAsPerView(), sortType)
+  }, [sortType, clothAsPerView])
 
   return (
     <main className="Main">
       <SideBar activeView={activeView}
-                setActiveView={setActiveView}
-                viewSort={viewSort}
-                setViewSort={setViewSort}
-    />
-    <div className="Main__catalog">
-      {sortedClothes.map((el: ClothItem) => (
-        <ProductCard item={el} key={el.name}/>
-      ))}
-    </div>
+               setActiveView={setActiveView}
+               viewSort={viewSort}
+               setViewSort={setViewSort}
+               sortType={sortType}
+               setSortType={setSortType}
+      />
+      <div className="Main__catalog">
+        {sortedClothes?.map((el: ClothItem) => (
+          <ProductCard item={el} key={el.name}/>
+        ))}
+      </div>
 
     </main>
   )
--- src/SideBar/sideBar.tsx
+++ src/SideBar/sideBar.tsx
@@ -1,11 +1,14 @@
 import React, {FC, useState} from "react";
-import './sidebar.scss'
+import './sidebar.scss';
+import cn from 'classnames';
 
 type Params = {
   activeView: string;
   setActiveView: (value: string) => void;
   viewSort: boolean;
   setViewSort: (value: boolean) => void;
+  sortType: string;
+  setSortType: (value: string) => void;
 }
 
 export const SideBar: FC<Params> = ({
@@ -13,6 +16,8 @@ export const SideBar: FC<Params> = ({
                                       setActiveView,
                                       viewSort,
                                       setViewSort,
+                                      sortType,
+                                      setSortType,
                                     }) => {
   const [view, setView] = useState(true)
   const handleActiveView = (target: string) => {
@@ -23,32 +28,87 @@ export const SideBar: FC<Params> = ({
     setViewSort(view);
   }
 
+  const handleSort = (value: string) => {
+    setSortType(value);
+    console.log(value)
+  }
+
   return (
     <aside className="SideBar">
       <ul className="SideBar__list">
-        <li className="SideBar__list_item" style={{fontWeight: activeView === 'All' ? 'bold' : 'lighter'}}
-            onClick={() => handleActiveView('All')}>Все
+        <li
+          className="SideBar__list_item"
+          style={{fontWeight: activeView === 'All' ? 'bold' : 'lighter'}}
+          onClick={() => handleActiveView('All')}
+        >
+          Все
         </li>
-        <li className="SideBar__list_item" style={{fontWeight: activeView === 'Coats' ? 'bold' : 'lighter'}}
-            onClick={() => handleActiveView('Coats')}>Плащи
+        <li
+          className="SideBar__list_item"
+          style={{fontWeight: activeView === 'Coats' ? 'bold' : 'lighter'}}
+          onClick={() => handleActiveView('Coats')}
+        >
+          Плащи
         </li>
-        <li className="SideBar__list_item" style={{fontWeight: activeView === 'Sneakers' ? 'bold' : 'lighter'}}
-            onClick={() => handleActiveView('Sneakers')}>Кроссовки
+        <li
+          className="SideBar__list_item"
+          style={{fontWeight: activeView === 'Sneakers' ? 'bold' : 'lighter'}}
+          onClick={() => handleActiveView('Sneakers')}
+        >
+          Кроссовки
         </li>
-        <li className="SideBar__list_item" style={{fontWeight: activeView === 'Shirts' ? 'bold' : 'lighter'}}
-            onClick={() => handleActiveView('Shirts')}>Рубашки
+        <li
+          className="SideBar__list_item"
+          style={{fontWeight: activeView === 'Shirts' ? 'bold' : 'lighter'}}
+          onClick={() => handleActiveView('Shirts')}
+        >
+          Рубашки
         </li>
-        <li className="SideBar__list_item" style={{fontWeight: activeView === 'Trousers' ? 'bold' : 'lighter'}}
-            onClick={() => handleActiveView('Trousers')}>Брюки
+        <li
+          className="SideBar__list_item"
+          style={{fontWeight: activeView === 'Trousers' ? 'bold' : 'lighter'}}
+          onClick={() => handleActiveView('Trousers')}
+        >
+          Брюки
         </li>
       </ul>
-      <button className="SideBar__sort" onClick={handleViewSort}>Сортировать <img src="https://raw.githubusercontent.com/vitaliikorol/store_test_task/master/public/images/icons/up-arrow.png" alt="&uarr;" style={{transform: viewSort ? 'rotate(180deg)' : ''}} className="SideBar__sort_img"/></button>
+      <button className="SideBar__sort" onClick={handleViewSort}>
+        Сортировать
+        <img
+        src="https://raw.githubusercontent.com/vitaliikorol/store_test_task/master/public/images/icons/up-arrow.png"
+        alt="&uarr;"
+        style={{transform: viewSort ? 'rotate(180deg)' : ''}}
+        className="SideBar__sort_img"/>
+      </button>
       <div className="SideBar__sort_wrapper">
-        <ul className="SideBar__list SideBar__sort_list" style={{top: viewSort ? '0' : '-190px'}}>
-          <li className="SideBar__list_item SideBar__sort_list-item">От дорогих к дешевым</li>
-          <li className="SideBar__list_item SideBar__sort_list-item">От дешевых к дорогим</li>
-          <li className="SideBar__list_item SideBar__sort_list-item">Популярные</li>
-          <li className="SideBar__list_item SideBar__sort_list-item">Новые</li>
+        <ul
+          className="SideBar__list SideBar__sort_list"
+          style={{top: viewSort ? '0' : '-190px'}}
+        >
+          <li
+            onClick={() => handleSort('Price_asc')}
+            className={cn(sortType === 'Price_asc' ? "SideBar__sort_list-item SideBar__sort_list-item-active" : "SideBar__sort_list-item")}
+          >
+            От дорогих к дешевым
+          </li>
+          <li
+            onClick={() => handleSort('Price_desc')}
+            className={cn(sortType === 'Price_desc' ? "SideBar__sort_list-item SideBar__sort_list-item-active" : "SideBar__sort_list-item")}
+          >
+            От дешевых к дорогим
+          </li>
+          <li
+            onClick={() => handleSort('Popularity')}
+            className={cn(sortType === 'Popularity' ? "SideBar__sort_list-item SideBar__sort_list-item-active" : "SideBar__sort_list-item")}
+          >
+            Популярные
+          </li>
+          <li
+            onClick={() => handleSort('Newest')}
+            className={cn(sortType === 'Newest' ? "SideBar__sort_list-item SideBar__sort_list-item-active" : "SideBar__sort_list-item")}
+          >
+            Новые
+          </li>
         </ul>
       </div>
     </aside>
